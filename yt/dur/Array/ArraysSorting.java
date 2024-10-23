c6f839f24a8cba49fae32460dc3c31e456d396dd
jhaaakash <aakashkumarjha295@gmail.com>
2024-10-23 23:14:16 +0530

    Adding new file in ArrayFolder

---
+++ yt/dur/Array/ArraysSorting.java
@@ -0,0 +1,16 @@
+package yt.dur.Array;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ArraysSorting {
+    public static void main(String[] args) {
+        int a=3;
+        int b=5;
+        int c=a+b;
+        System.out.println("a + b is "+ c);
+
+
+
+    }
+}

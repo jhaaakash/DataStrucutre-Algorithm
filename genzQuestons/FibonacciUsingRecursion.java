4b78e024dec8e2a23c582a6ed169f9b3017dd72d
jhaaakash <aakashkumarjha295@gmail.com>
2024-10-28 22:05:22 +0530

    Fibonacci Series Code

---
+++ genzQuestons/FibonacciUsingRecursion.java
@@ -0,0 +1,27 @@
+public class FibonacciUsingRecursion {
+
+    private static int fibonacci(int n) {
+        
+        if(n<1)
+        {
+            return 0;
+        }
+        if(n==1)
+        {
+            return 1;
+        }
+        else{
+            return fibonacci(n-1) + fibonacci(n-2);
+        }
+        
+    }
+
+    
+    public static void main(String[] args) {
+        int numberInput=10;
+        int output= fibonacci(numberInput);
+        System.out.println(output);
+    }
+
+   
+}

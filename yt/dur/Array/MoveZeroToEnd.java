5933a6e5ae9de040eae1dc9508ad72e9223e4fb2
jhaaakash <aakashkumarjha295@gmail.com>
2024-11-02 10:35:35 +0530

    MovingZeroToEndAlgorithm

---
+++ yt/dur/Array/MoveZeroToEnd.java
@@ -0,0 +1,61 @@
+package yt.dur.Array;
+
+import java.util.Scanner;
+
+public class MoveZeroToEnd {
+
+    public static void moveAllZeroToEnd(int number[])
+    {
+        int left=0;
+        int right=number.length-1;
+        while (left<right) {
+            if(number[left]==0 && number[right]!=0)
+            {
+                number[left]=number[right];
+                number[right]=0;
+                left++;
+                right--;
+            }
+            else if(number[left]==0 && number[right]==0)
+            {
+                right--;
+            }
+            else if(number[left]!=0 && number[right]!=0)
+            {
+                left++;
+            }
+            else{
+                left++;
+                right--;
+            }
+            
+        }
+        System.out.println("the arrya with zero at the end is:");
+        for (int i : number) {
+            System.out.print(i+"\t");
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of Elements in an Array");
+        int size= sc.nextInt();
+        System.out.println("Enter the " + size +" Elements of the array including zeros");
+        int inputNumber[]= new int[size];
+
+        for(int i=0; i<size; i++)
+        {
+            inputNumber[i]= sc.nextInt();
+        }
+
+        System.out.println(" the input number array is");
+        for (int i : inputNumber) {
+            System.out.print(i+"\t");
+        }
+        System.out.println();
+        moveAllZeroToEnd(inputNumber);
+
+
+    }
+}

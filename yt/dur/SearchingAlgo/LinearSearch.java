0bb646f48260ec94dad40caf358d74e0e699c764
jhaaakash <aakashkumarjha295@gmail.com>
2024-11-04 09:30:18 +0530

    LinearSearch

--- yt/dur/SearchingAlgo/LinearSearch.java
+++ yt/dur/SearchingAlgo/LinearSearch.java
@@ -1,5 +1,42 @@
 package yt.dur.SearchingAlgo;
 
+import java.util.Scanner;
+
 public class LinearSearch {
+
+    public static int linerSearchfunction(int array[], int target)
+    {
+        int index=-1;
+        for ( int i=0; i<array.length;i++)
+        {
+            if(array[i]==target)
+            {
+                index=i;
+                break;
+            }
+        }
+        return index;
+    }
+
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter thesize of the array:");
+        int size= sc.nextInt();
+        int numbers[] = new int[size];
+        System.out.println("Enter the "+size+" numbers");
+        for(int i=0; i<size; i++)
+        {
+            numbers[i]=sc.nextInt();
+        }
+
+        System.out.println("Enter the target element now");
+        int target= sc.nextInt();
+
+        int foundIndex=linerSearchfunction(numbers, target);
+        System.out.println("Element found at index "+foundIndex);
+
+    }
     
 }

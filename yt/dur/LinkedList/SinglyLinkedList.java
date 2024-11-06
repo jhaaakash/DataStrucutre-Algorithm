a8f7a5fdbbbcc4e684b6bdf656754e394a683754
jhaaakash <aakashkumarjha295@gmail.com>
2024-11-06 09:31:06 +0530

    LinkedListOperation.java

--- yt/dur/LinkedList/SinglyLinkedList.java
+++ yt/dur/LinkedList/SinglyLinkedList.java
@@ -16,8 +16,25 @@ public class SinglyLinkedList {
         }
 
         //insertat last
+
+        public void insertAtEnd(int item)
+        {
+            Node newNode= new Node(item);
+            if(head==null)
+            {
+                head= newNode;
+                return;
+            }
+            // importat to keep our head intact and track the tail
+            Node temp = head;
+            while (temp.next!=null) {
+                temp=temp.next;
+            }
+            temp.next =newNode;
+        }
         
- 
+        // insert at specific position
+        
         
     }
 

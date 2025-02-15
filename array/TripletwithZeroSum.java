cca9960b9ed3482c3d02d3be9ec356e3744db63c
jhaaakash <aakashkumarjha295@gmail.com>
2025-02-15 10:35:58 +0530

    triplet sum 0 approach 1

---
+++ array/TripletwithZeroSum.java
@@ -0,0 +1,30 @@
+package array;
+
+public class TripletwithZeroSum {
+    public static void main(String[] args) {
+        int arr[]={0, -1, 2, -3, 1};
+        int size=arr.length;//5
+        int target=0;
+
+/* first approach
+        for(int i=0;i<=size-3; i++)
+        {
+            for(int j=i+1;j<=size-2; j++)
+            {
+                for(int k=j+1;j<=size-1; k++)
+                {
+                    if(arr[i]+arr[j]+arr[k]==0)
+                    {
+                        System.out.println("The triplet is:" +arr[i]+ " " +arr[j]+ " "+arr[k]);
+                    }
+                }
+            }
+        }
+    */
+
+    //Second Approach:
+    
+
+        
+    }
+}

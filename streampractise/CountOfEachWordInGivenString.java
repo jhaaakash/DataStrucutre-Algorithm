49f177d2e5992b122e22b002909c86b84f00c2f1
jhaaakash <aakashkumarjha295@gmail.com>
2025-02-16 14:33:28 +0530

    SeparatingKeyValueAndItsCount

---
+++ streampractise/CountOfEachWordInGivenString.java
@@ -0,0 +1,8 @@
+package streampractise;
+
+public class CountOfEachWordInGivenString {
+    public static void main(String[] args) {
+        String name="My Nmae is Aakash Kumar Jha. I am a Java Developer. I am Aakash Kumar";
+        
+    }
+}
--- streampractise/SeparatingKeyValueAndItsCount.java
+++ streampractise/SeparatingKeyValueAndItsCount.java
@@ -8,12 +8,21 @@ import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 public class SeparatingKeyValueAndItsCount {
-    
+
+    public static void main(String[] args) {
     String[] lst={"Vegetables-Okra", "Vegetables-Spinach","Fruits-Apple", "Fruits-Banana", "Fruits-Grapes"};
     List<String> vegAndFruit= Arrays.asList(lst);
-    Map<String, String> keyValue= 
-    vegAndFruit.stream().collect(Collectors.groupingBy(a ->a.split("-")[0],Collectors.mapping(a-> a.split("-")[1],Collectors.joining(","))));
-    //forEach((k, v) -> System.out.println(k + " " + v));
-    keyValue.stream().forEach((Key, Value) -> System.out.println(key +" "+Value));
-    
+    Map<String, String> keyValue = 
+    vegAndFruit.stream().collect(
+        Collectors.groupingBy(
+                               a -> a.split("-")[0],
+                                                          Collectors.mapping(
+                                                                             a -> a.split("-")[1],
+                                                                             Collectors.joining(",")
+                                                                            )
+                             )
+                              );
+
+      keyValue.forEach((key, value) -> System.out.println(key + " -> " + value));
+                            }
 }

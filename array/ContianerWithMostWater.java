e00d1715114ae964884abc3b705a9613596c5b75
jhaaakash <aakashkumarjha295@gmail.com>
2025-02-17 21:16:14 +0530

    ContianerWithMostWater2

--- array/ContianerWithMostWater.java
+++ array/ContianerWithMostWater.java
@@ -5,9 +5,13 @@ public class ContianerWithMostWater {
         int[] arr = { 1, 8, 6, 2, 5, 4, 8, 3, 7 }; // Example array
         int result = maxArea(arr);
                 System.out.println("Maximum area: " + result);
-            }
+        int result1= maxArea1(arr);
+        System.out.println("Maximum area: " + result1);
+                    }
+                
+             
         
-            private static int maxArea(int[] arr) {
+                    private static int maxArea(int[] arr) {
                 int area=0;
                 int maxWidth = arr.length;
                 for(int i=0; i< maxWidth-1; i++){
@@ -19,11 +23,27 @@ public class ContianerWithMostWater {
                         area = Math.max(area, tempArea);
                     }
                     
-                }
-              
-              
-              
-              return  area;
-                
+                }  
+              return  area;      
             }
+
+            //Second Approach
+            private static int maxArea1(int[] arr) {
+                int area=0;
+                int left = 0;
+                int right = arr.length-1;
+                while(left<right){
+                    int height = Math.min(arr[left], arr[right]);
+                    int width = right - left;
+                    int tempArea = height * width;
+                    area = Math.max(area, tempArea);
+                    if(arr[left]<arr[right]){
+                        left++;
+                    }else{
+                        right--;
+                    }
+                }
+                return area;
+            }
+
 }
